5916ad6595c46109e9b2c7802ffdea40d1eed0b4
Kraykin <kraykin@gmail.com>
2016-02-12 20:35:20 +0300

    Add articles route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
