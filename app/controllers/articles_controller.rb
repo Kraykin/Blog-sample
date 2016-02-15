cad7ecaf22663e2a718136561e76195d44b2e8dc
Sergey Kraykin <kraykin@gmail.com>
2016-02-15 19:37:16 +0100

    Create articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,15 @@ class ArticlesController < ApplicationController
     @article = Article.find params[:id]
   end
 
+  def update
+    @article = Article.find params[:id]
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
   private
 
   def article_params # allowing attributes from params[]
