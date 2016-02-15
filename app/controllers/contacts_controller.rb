f212fe3100e6be54f2d33ff2810c647c77d42d06
Sergey Kraykin <kraykin@gmail.com>
2016-02-15 11:05:46 +0100

    Get params from 'contacts/new' view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,13 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new contact_params
+  end
+
+  private
+
+  def contact_params # allowing attributes from params[]
+    params.require(:contact).permit(:email, :message)
   end
 
 end
