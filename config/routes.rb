351e628f916c4829272dc47030fb1e04246076d0
Sergey Kraykin <kraykin@gmail.com>
2016-02-15 14:54:47 +0100

    Change route from '/abouts to '/about'

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :articles
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resource :terms, only: [:show], path_names: { :show => '' }
-  resource :abouts, only: [:show], path_names: { :show => '' }
+  resource :about, only: [:show], path_names: { :show => '' }
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
