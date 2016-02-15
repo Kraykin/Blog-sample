b8da9889d621fcd2c82a0cc83ddafba2a2854d7a
Sergey Kraykin <kraykin@gmail.com>
2016-02-15 15:17:13 +0100

    Validate and save new article to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,18 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @atricle = Article.new atricle_params
+    if @atricle.valid?
+      @atricle.save
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def atricle_params # allowing attributes from params[]
+    params.require(:article).permit(:title, :text)
   end
 
 end
