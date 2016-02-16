f9bbae304dc0e55519979beaa968478dbbd97c5a
Sergey Kraykin <kraykin@gmail.com>
2016-02-16 16:19:01 +0100

    Create action articles#destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,14 +5,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find params[:id]
+    @article = Article.find(params[:id])
   end
   
   def new
   end
 
   def create
-    @article = Article.new article_params
+    @article = Article.new(article_params)
     if @article.valid?
       @article.save
       redirect_to @article
@@ -22,16 +22,20 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find params[:id]
+    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find params[:id]
+    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
       render action: 'edit'
     end
+  end
+
+  def destroy
+
   end
 
   private
