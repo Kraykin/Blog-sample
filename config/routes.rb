3a566419387c5e3c6179372eae1dec25834d26ff
Sergey Kraykin <kraykin@gmail.com>
2016-02-16 15:30:17 +0100

    Revert "Add route to pages#payment"
    This reverts commit 9f38892f46c736286e681172baa3612567c4f7e0.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'payment' => 'pages#payment'
-
+  
+  resources :articles
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resource :terms, only: [:show], path_names: { :show => '' }
   resource :about, only: [:show], path_names: { :show => '' }
-  resources :articles
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
