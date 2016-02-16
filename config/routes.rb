5fb1c3ab9069892656cd74df0153bff2261ca35c
Sergey Kraykin <kraykin@gmail.com>
2016-02-16 16:46:54 +0100

    Delete controller 'abouts'

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resource :terms, only: [:show], path_names: { :show => '' }
-  resource :about, only: [:show], path_names: { :show => '' }
   resources :articles
   
   # The priority is based upon order of creation: first created -> highest priority.
