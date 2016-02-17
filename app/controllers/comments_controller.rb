a5f9fb561a6526411afef89c0a1eb6f2047aa5d6
Sergey Kraykin <kraykin@gmail.com>
2016-02-17 13:07:33 +0100

    Added action comments#create and record comment in db

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  
+  def create
+    @article = Article.find(params[:article_id])
+    @article.comments.create(comment_params)
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def comment_params # allowing attributes from params[]
+    params.require(:comment).permit(:author, :body)
+  end
+
 end
