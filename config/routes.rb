74a9ca294b095a36652cf8c7522960fde858db38
Sergey Kraykin <kraykin@gmail.com>
2016-02-18 14:02:37 +0100

    Configuring for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
+  root to: "home#index"
+
   get 'home/index'
-  get '/' => 'articles#index'
+  
   get 'payment' => 'pages#payment'
   get 'about' => 'pages#about'
 
