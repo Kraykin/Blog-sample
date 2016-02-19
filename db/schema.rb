a8094df8c92074464ab6255fd314338cec12c322
Sergey Kraykin <kraykin@gmail.com>
2016-02-19 08:17:22 +0100

    Run db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219062344) do
+ActiveRecord::Schema.define(version: 20160219071003) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -55,5 +55,6 @@ ActiveRecord::Schema.define(version: 20160219062344) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
