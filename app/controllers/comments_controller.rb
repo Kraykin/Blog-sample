1b3e3f26a6d8c612d96a807f9419db6113602b04
Sergey Kraykin <kraykin@gmail.com>
2016-02-19 10:25:17 +0100

    Change ':author' to '.username' when create new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,16 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
-
+    comment = @article.comments.new(comment_params)
+    comment[:author] = current_user.username
+    comment.save
     redirect_to article_path(@article)
   end
 
   private
 
   def comment_params # allowing attributes from params[]
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
 
 end
