a5744cc0c4d9c7bdc581dd0b334d536ad54bc2be
Sergey Kraykin <kraykin@gmail.com>
2016-02-22 15:05:35 +0100

    Record article author to table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
+    @article[:author_id] = current_user.id
     if @article.valid?
       @article.save
       redirect_to @article
