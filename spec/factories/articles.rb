564709cac675452357d2a04080151f134fd875ab
Sergey Kraykin <kraykin@gmail.com>
2016-02-23 14:22:15 +0300

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :article do
     title 'Article title'
     text 'Article text'
+
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        create_list :comment, 3, article: article
+      end
+    end
+    
   end
 end
