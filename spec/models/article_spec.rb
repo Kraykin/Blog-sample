376866a46b49e1c11feba3c2163e2b2bf5c56dd6
Sergey Kraykin <kraykin@gmail.com>
2016-02-23 14:55:37 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,8 @@ describe Article do
   describe "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
+    it { should ensure_length_of(:title).is_at_most(140) }
+    it { should ensure_length_of(:text).is_at_most(4000) }
   end
   
   describe "associations" do
