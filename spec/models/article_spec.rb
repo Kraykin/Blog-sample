add49622b207fec2d366c99374852dd69c7b87cf
Sergey Kraykin <kraykin@gmail.com>
2016-02-23 13:35:52 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Article do
+  
   describe "validations" do
     it { should validate_presence_of :title }
-    it { should validate_presence_of :text }  
+    it { should validate_presence_of :text }
   end
   
   describe "associations" do
-    it { should have_many :comments }  
+    it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Test article title')
+      expect(article.subject).to eq 'Test article title'
+    end
+  end
+  
 end
