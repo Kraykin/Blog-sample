3521e652996ee9ba4bb7ee5193a9b283f55b7b3b
Sergey Kraykin <kraykin@gmail.com>
2016-02-23 19:15:51 +0300

    Update contact acceptance test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 feature "Contact Creation" do
   scenario "allows access to contacts page" do
     visit '/contacts'
-    expect(page).to have_content 'Message'
+    expect(page).to have_content I18n.t 'contacts.message'
   end
 end
