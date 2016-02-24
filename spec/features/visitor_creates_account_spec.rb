c4d22c831b1960c395dd2ee7e6c970a699dee9fc
Sergey Kraykin <kraykin@gmail.com>
2016-02-24 11:16:20 +0300

    Add account creation test

---
+++ spec/features/visitor_creates_account_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "Account Creation" do
+  scenario "allows guest to create account" do
+    visit new_user_registration_path
+    
+    fill_in :user_email, :with => 'user@example.com'
+    fill_in :user_username, :with => 'Test_new_user'
+    fill_in :user_password, :with => 'Secure123!@#'
+    fill_in :user_password_confirmation, :with => 'Secure123!@#' # use name from id="user_password_confirmation"
+    click_button 'Sign up'
+    
+    expect(page).to have_content I18n.t 'devise.registrations.signed_up'
+  end
+end
