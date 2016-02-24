b979a3c7648a0952be1d20bb89f6aa6d09f28e1d
Sergey Kraykin <kraykin@gmail.com>
2016-02-24 16:02:30 +0300

    Add article editing test

--- spec/features/user_edits_article_spec.rb
+++ spec/features/user_edits_article_spec.rb
@@ -11,4 +11,11 @@ feature "Article Editing" do
     expect(page).to have_content I18n.t('edit_article.subject')
   end
 
+  scenario "allows user to editing article" do
+    fill_in :article_text, :with => 'Test editing article text'
+    click_button 'Save article'
+
+    expect(page).to have_content 'Test editing article text'
+  end
+
 end
