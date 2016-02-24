4e3964f4d5cdd413fe37a6076997db7795610437
Sergey Kraykin <kraykin@gmail.com>
2016-02-24 14:32:37 +0300

    Add article creation test

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -6,7 +6,16 @@ feature "Article Creation" do
   end
 
   scenario "allows user to visit new article page" do
-    visit new_article_path    
+    visit new_article_path
     expect(page).to have_content I18n.t('new_article.subject')
+  end
+
+  scenario "allows user to create new article" do
+    visit new_article_path
+    fill_in :article_title, :with => 'Test article title'
+    fill_in :article_text, :with => 'Test article text'
+    click_button 'Create article'
+
+    expect(page).to have_content 'Test article title'
   end
 end
